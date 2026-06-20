74c1c108b36e7ad27389eab18c2a9ece8b7080e8
“hadrian <hadrianbelove@gmail.com>
2026-06-19 17:32:59 -0700

    Capsule + pull quote + NOT SLOP: This Is Buzz APPROVED: DELETE

--- scripts/slop_classifier.py
+++ scripts/slop_classifier.py
@@ -33,6 +33,7 @@ MANUAL_OVERRIDES = {
     1684246: False,   # Untold UK: Vinnie Jones
     # Art-house / festival films without RT/wiki yet
     1213025: False,   # Transcending Dimensions (Toshiaki Toyoda; Rotterdam/NYAFF/Fantasia)
+    1362365: False,   # This Is Buzz (Mark Pellington doc; Slamdance)
     1303370: False,   # Ways to Traverse a Territory
     1422627: False,   # Snow Leopard Sisters
     1303498: False,   # $POSITIONS
